e17420972566d605f5fe48f35a8e47846ab1dbb2
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-08-22 10:52:58 -0700

    Add Arry Method

---
+++ app.js
@@ -0,0 +1,55 @@
+// Get the DOM element where you want to print the content
+// const outputContainer = document.getElementById('output');
+
+// // Content to print
+// const content = 'I Love You "❤️"';
+
+// // Number of times to print
+// const numberOfPrints = 100;
+
+// // Loop to print the content multiple times
+// for (let i = 0; i < numberOfPrints; i++) {
+//   const contentElement = document.createElement('p');
+//   contentElement.textContent = content;
+//   outputContainer.appendChild(contentElement);
+// }
+
+
+// const fruit = ["Apple","Mango","Banana","Orange"];
+// // console.log(fruit)
+// // fruit.push("Peach")
+// // fruit.pop()
+// // fruit.shift("")
+// // fruit.unshift("Water Mallon")
+// // const newarry = fruit.slice(1,4)
+// // console.log(newarry)
+// // fruit.splice(0,1,"Water Mallon","Nashpati")
+// fruit.
+// console.log(fruit)
+
+// const names = "My Name Is Ahsan Ali And My Age is 20"
+// const newArry = names.split(" ")
+// newArry [3] = "Hasnain"
+// newArry [9] = "18"
+// // newArry.splice(5)
+// // console.log (names)
+// const newStr = newArry.join(" ")
+// console.log (newArry)
+// console.log (newStr)
+
+
+
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+// let newArr = array1.concat(array2);
+// console.log(newArr);
+// newArr.reverse();
+// console.log(newArr);
+
+
+
+// let arr = ["q", "w", "e", "r", "t", "y", "u", "i", "o", " p", "a", "s", "d", "f", 'g', 'h', "j", 'k', "l", " z", "x", "c", "v", "b", "n", "m"];
+// const newArry = arr.sort();
+// console.log(newArry);
+// const indexNumber = arr.indexOf(26);
+// console.log(indexNumber);
